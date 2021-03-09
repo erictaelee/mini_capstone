5b228035d623f47f5b9bbb9ca589df0187b4fea5
Eric <lee.erictae@gmail.com>
2021-03-08 18:44:49 -0600

    create url model to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,10 @@ class Product < ApplicationRecord
   validates :description, length: { in: 10..500 }
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0 }
+
+  def supplier
+    Supplier.find_by(id: supplier_id)
+  end
   
   def is_discounted
     if price < 10
